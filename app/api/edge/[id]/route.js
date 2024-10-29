f8103b3cc9c552c3feb8234be5cc91d7ed0c194e
mizan21 <mizanorrahman398@gmail.com>
2024-10-29 11:48:54 +0600

    Edge part added

--- app/api/edge/[id]/route.js
+++ app/api/edge/[id]/route.js
@@ -1,4 +1,5 @@
 import { connectToDB } from "@/app/lib/connectToDB";
+import { Edge } from "@/app/lib/Edge/model";
 import { Teams } from "@/app/lib/Teams/model";
 import { NextResponse } from "next/server";
 
@@ -9,26 +10,26 @@ export async function PATCH(req, { params }) {
   await connectToDB();
 
   try {
-    const updatedJob = await Teams.findByIdAndUpdate(id, updateData, {
+    const updatedEdge = await Edge.findByIdAndUpdate(id, updateData, {
       new: true, // Returns the updated document
       runValidators: true, // Ensures model validation
     });
 
-    if (!updatedJob) {
+    if (!updatedEdge) {
       return NextResponse.json(
-        { message: "team data not found" },
+        { message: "edge data not found" },
         { status: 404 }
       );
     }
 
     return NextResponse.json(
-      { message: "Data Successfully Updated", data: updatedJob },
+      { message: "Data Successfully Updated", data: updatedEdge },
       { status: 200 }
     );
   } catch (error) {
-    console.error("Failed to update team data:", error);
+    console.error("Failed to update edge data:", error);
     return NextResponse.json(
-      { message: "Failed to update team data" },
+      { message: "Failed to update edge data" },
       { status: 500 }
     );
   }
--- app/api/edge/route.js
+++ app/api/edge/route.js
@@ -1,50 +1,49 @@
 import { connectToDB } from "@/app/lib/connectToDB";
+import { Edge } from "@/app/lib/Edge/model";
 import { Teams } from "@/app/lib/Teams/model";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   await connectToDB();
-  const teams = await Teams.find();
+  const teams = await Edge.find();
   return NextResponse.json(teams);
 }
 
 export async function POST(req) {
   try {
-    const { id, image, name, title, isActive = true } = await req.json();
+    const edgeData = await req.json();
 
     // Connect to the database
     await connectToDB();
-
-    // Create a new job document
-    await Teams.create({ id, image, name, title, isActive });
-
-    return NextResponse.json({ message: "team data created" }, { status: 201 });
+    await Edge.create(edgeData);
+    return NextResponse.json({ message: "Edge data created" }, { status: 201 });
   } catch (error) {
-    console.error("Error creating team data:", error);
+    console.error("Error creating edge data:", error);
     return NextResponse.json(
-      { message: "Failed to create team data" },
+      { message: "Failed to create edge data" },
       { status: 500 }
     );
   }
 }
+
 export async function DELETE(req) {
   try {
     const url = new URL(req.url);
     const id = url.searchParams.get("id");
     await connectToDB();
-    const deletedTeam = await Teams.findByIdAndDelete(id);
-    if (!deletedTeam) {
+    const deletedEdge = await Edge.findByIdAndDelete(id);
+    if (!deletedEdge) {
       return NextResponse.json(
-        { message: "Team data not found" },
+        { message: "Edge data not found" },
         { status: 404 }
       );
     }
 
-    return NextResponse.json({ message: "team data deleted" }, { status: 200 });
+    return NextResponse.json({ message: "edge data deleted" }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { message: "Failed to delete team data" },
+      { message: "Failed to delete edge data" },
       { status: 500 }
     );
   }
