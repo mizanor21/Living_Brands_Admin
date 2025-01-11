c0607d7bb9bad3ebc5a995c544d7d9dc2ccf85d0
mizan21 <mizanorrahman398@gmail.com>
2025-01-11 12:39:43 +0600

    Add SWR for data fetching, implement news filtering, and enhance error handling in API routes

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+// server.js
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3300; // Use port from environment or default to 3000
-const app = next({ dev });
+//process.env.NODE_ENV !== 'production'
+
+const dev = false;
+const hostname = "localhost";
+const port = process.env.port || 3100;
+// when using middleware hostname and port must be provided below
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass true as the second argument to url.parse.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log("> Ready on http://${hostname}:${port}");
+    });
 });
